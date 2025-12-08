480f75428cd0dcf521617880961d382ba1bf9c61
KP12130 <kpwork12130@gmail.com>
2025-12-08 17:54:48 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    TOKEN: process.env.DISCORD_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GUILD_ID: process.env.GUILD_ID,
+    TICKET_CATEGORY_ID: process.env.TICKET_CATEGORY_ID,
+    STAFF_ROLE_ID: process.env.STAFF_ROLE_ID,
+    INVITE_LOG_CHANNEL: process.env.INVITE_LOG_CHANNEL,
+    MEMBER_COUNT_CHANNEL: process.env.MEMBER_COUNT_CHANNEL
+};
